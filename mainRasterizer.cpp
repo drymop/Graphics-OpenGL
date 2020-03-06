870be35571c98ff99ac14744a026aab7475c4769
lmtuan <lmtuan98@gmail.com>
2020-03-05 21:02:04 -0500

    Mouse movement

--- mainRasterizer.cpp
+++ mainRasterizer.cpp
@@ -33,6 +33,7 @@
 #include "RasterizableObject.h"
 #include "ObjFileParser.h"
 
+using glm::vec2, glm::vec3, glm::vec4, glm::mat4;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Global variables - avoid these
@@ -51,6 +52,13 @@ std::chrono::high_resolution_clock::time_point g_frameTime{
 float g_delay{0.f};
 float g_framesPerSecond{0.f};
 
+// Antialiasing
+bool g_antiAliasing = false;
+
+// Scene
+vec3 eye, at, up, right;
+mat4 projMatrix;
+
 std::unique_ptr<RasterizableObject> g_obj;
 std::unique_ptr<RasterizableObject> g_obj2;
 
@@ -61,12 +69,16 @@ std::unique_ptr<RasterizableObject> g_obj2;
 /// @brief Initialize GL settings
 void
 initialize() {
-  using glm::vec3, glm::vec4, glm::mat4;
-  using glm::translate, glm::scale, glm::transpose, glm::inverse;
+  using glm::translate, glm::scale, glm::transpose, glm::inverse, glm::cross;
 
   glClearColor(0.f, 0.f, 0.0f, 0.f);
   glEnable(GL_COLOR_MATERIAL);
   glEnable(GL_DEPTH_TEST);
+  if (g_antiAliasing) {
+    glEnable(GL_MULTISAMPLE_ARB);
+  } else {
+    glDisable(GL_MULTISAMPLE_ARB);
+  }
 
   g_program = compileProgram("shaders/phong.vert",
                              "shaders/phong.frag");
@@ -78,12 +90,13 @@ initialize() {
   // Scene
 
   // Set up camera
-  glUniform3f(glGetUniformLocation(g_program, "cameraPos"), 0, 0, 0);
-  vec3 eye(0.f, 0.f, 0.f);
-  vec3 center(0.f, 0.f, -1.f);
-  vec3 up(0.f, 1.f, 0.f);
-  mat4 viewMatrix = glm::lookAt(eye, center, up);
-  mat4 projMatrix = glm::perspective(glm::radians(60.f), g_width/((float)g_height), 0.1f, 100.f);
+  eye = {0.f, 0.f, 0.f};
+  at = {0.f, 0.f, -1.f};
+  up = {0.f, 1.f, 0.f};
+  right = cross(at, up);
+  glUniform3f(glGetUniformLocation(g_program, "cameraPos"), eye.x, eye.y, eye.z);
+  mat4 viewMatrix = glm::lookAt(eye, eye + at, up);
+  projMatrix = glm::perspective(glm::radians(60.f), g_width/((float)g_height), 0.1f, 100.f);
   glUniformMatrix4fv(
       glGetUniformLocation(g_program, "viewProjectionMatrix"), 
       1, GL_FALSE, glm::value_ptr(projMatrix * viewMatrix));
@@ -175,6 +188,13 @@ draw() {
   // Clear
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+  // Set up camera
+  glUniform3f(glGetUniformLocation(g_program, "cameraPos"), eye.x, eye.y, eye.z);
+  mat4 viewMatrix = glm::lookAt(eye, eye + at, up);
+  glUniformMatrix4fv(
+      glGetUniformLocation(g_program, "viewProjectionMatrix"), 
+      1, GL_FALSE, glm::value_ptr(projMatrix * viewMatrix));
+
   //////////////////////////////////////////////////////////////////////////////
   // Draw
   g_obj->draw();
@@ -207,6 +227,17 @@ keyPressed(GLubyte _key, GLint _x, GLint _y) {
       glutDestroyWindow(g_window);
       g_window = 0;
       break;
+    case 'a':
+      g_antiAliasing = !g_antiAliasing;
+      std::cout << "Anti-aliasing: " << g_antiAliasing << std::endl;
+      if (g_antiAliasing) {
+        glEnable(GL_MULTISAMPLE_ARB);
+        glHint(GL_MULTISAMPLE_FILTER_HINT_NV, GL_NICEST);
+      } else {
+        glDisable(GL_MULTISAMPLE_ARB);
+      }
+      glutPostRedisplay();
+      break;
     // Unhandled
     default:
       std::cout << "Unhandled key: " << (int)(_key) << std::endl;
@@ -233,6 +264,84 @@ specialKeyPressed(GLint _key, GLint _x, GLint _y) {
   }
 }
 
+const int GLUT_NO_BUTTON = -1;
+const int GLUT_WHEEL_UP = 3;
+const int GLUT_WHEEL_DOWN = 4;
+
+// only 1 mouse button can be active, other will be ignored
+int g_activeDragButton = GLUT_NO_BUTTON;
+int g_mouseStartX;
+int g_mouseStartY;
+vec3 g_eyeStart;
+float g_scale = 0.02f;
+float g_zScale = 0.2f;
+
+vec3 g_atStart, g_upStart, g_rightStart;
+float g_angleDelta = 0.005f;
+
+void 
+mousePressed(int button, int state, int x, int y) {
+  std::cout << "Button " << button << " active " << g_activeDragButton << std::endl;
+  if (button == g_activeDragButton) {
+    // release the active button
+    g_activeDragButton = GLUT_NO_BUTTON;
+    return;
+  } else if (g_activeDragButton != GLUT_NO_BUTTON) {
+    // ignore other button if there's already an active button
+    return;
+  }
+  switch(button) {
+    case GLUT_LEFT_BUTTON:
+      g_activeDragButton = GLUT_LEFT_BUTTON;
+      g_mouseStartX = x;
+      g_mouseStartY = y;
+      g_eyeStart = eye;
+      break;
+    case GLUT_RIGHT_BUTTON:
+      g_activeDragButton = GLUT_RIGHT_BUTTON;
+      g_mouseStartX = x;
+      g_mouseStartY = y;
+      g_atStart = at;
+      g_upStart = up;
+      g_rightStart = right;
+      break;
+    case GLUT_WHEEL_UP:
+      eye += at * g_zScale;
+      break;
+    case GLUT_WHEEL_DOWN:
+      eye -= at * g_zScale;
+      break;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Callback function for mouse actions
+void
+mouseDragged(int x, int y) {
+  if(g_activeDragButton == GLUT_LEFT_BUTTON) {
+    float dx = x - g_mouseStartX;
+    float dy = y - g_mouseStartY;
+    // change camera position
+    eye = g_eyeStart + (right * -dx + up * dy) * g_scale;
+  } else if(g_activeDragButton == GLUT_RIGHT_BUTTON) {
+    float dx = x - g_mouseStartX;
+    float dy = g_mouseStartY - y;
+    vec2 delta(-dy, dx);
+    float d = glm::length(delta);
+    delta = glm::normalize(delta);
+    
+    std::cout << dx << " " << dy << " "<< d << std::endl;
+
+
+    vec3 rotationAxis = g_rightStart * delta.x + g_upStart * delta.y;
+    float rotationAngle = d * g_angleDelta;
+    mat4 rotationMatrix = glm::rotate(mat4(1.f), rotationAngle, rotationAxis);
+    at = rotationMatrix * vec4(g_atStart, 1.f);
+    up = rotationMatrix * vec4(g_upStart, 1.f);
+    right = glm::cross(at, up);
+  }
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Main
 
@@ -250,7 +359,8 @@ main(int _argc, char** _argv) {
   glutInit(&_argc, _argv);
   glutInitContextVersion(3, 3);
   glutInitContextProfile(GLUT_CORE_PROFILE);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutSetOption(GLUT_MULTISAMPLE, 8);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH | GLUT_MULTISAMPLE);
   glutInitWindowPosition(50, 100);
   glutInitWindowSize(g_width, g_height); // HD size
   g_window = glutCreateWindow("Spiderling: A Rudamentary Game Engine");
@@ -265,6 +375,9 @@ main(int _argc, char** _argv) {
   glutDisplayFunc(draw);
   glutKeyboardFunc(keyPressed);
   glutSpecialFunc(specialKeyPressed);
+  glutMouseFunc(mousePressed);
+  glutMotionFunc(mouseDragged);
+  glutPassiveMotionFunc(NULL);
   glutTimerFunc(1000/FPS, timer, 0);
 
   // Start application
